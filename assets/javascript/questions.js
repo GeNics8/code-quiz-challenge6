002186ddca9664b7daaf96a14c37b8dcc42e261f
Vi <m2107313@middlesbro.ac.uk>
2023-02-13 19:51:48 +0000

    changes to the files

---
+++ assets/javascript/questions.js
@@ -0,0 +1,33 @@
+// an array of objects that will re. the questions 
+// const & let instead of var 
+// square brackets 
+// JSON = javascript object notation (a file in the form of an array of objects)
+// objects have a key and value pairs 
+var questions = [
+    
+{
+    title: "What do all objects have?",
+    choice: ['key & value pairs', 'true or false', 'etc', 'etc'],
+    answer: 'key & value pairs',
+
+},
+{
+    title: "What do all objects have?",
+    choice: ['key & value pairs', 'true or false', 'etc', 'etc'],
+    answer: 'key & value pairs',
+
+},
+{
+    title: "What do all objects have?",
+    choice: ['key & value pairs', 'true or false', 'etc', 'etc'],
+    answer: 'key & value pairs',
+
+},
+{
+    title: "What do all objects have?",
+    choice: ['key & value pairs', 'true or false', 'etc', 'etc'],
+    answer: 'key & value pairs',
+
+},
+
+]
